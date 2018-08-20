9603aae4f756accfcf9c0bc1717afb080b3f41f5
Nick Sanford <nicholascsanford@gmail.com>
2018-08-20 07:10:21 -0400

    Add profiling to master branch

---
+++ wc-haskell/profile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+stack clean
+stack build --profile
+rm wc-haskell-exe.eventlog wc-haskell-exe.dashs wc-haskell-exe.prof
+stack exec -- wc-haskell-exe ../tmp +RTS -l # generate eventlog
+stack exec -- wc-haskell-exe ../tmp +RTS -p # generate profile
+stack exec -- wc-haskell-exe ../tmp +RTS -s 2> wc-haskell-exe.dashs
