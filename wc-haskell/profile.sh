9d4245c49d5769964b3f2232bb9a4bbd84a92ace
Nick Sanford <nicholascsanford@gmail.com>
2018-08-20 07:31:02 -0400

    Add output to profiling

--- wc-haskell/profile.sh
+++ wc-haskell/profile.sh
@@ -5,3 +5,4 @@ rm wc-haskell-exe.eventlog wc-haskell-exe.dashs wc-haskell-exe.prof
 stack exec -- wc-haskell-exe ../tmp +RTS -l # generate eventlog
 stack exec -- wc-haskell-exe ../tmp +RTS -p # generate profile
 stack exec -- wc-haskell-exe ../tmp +RTS -s 2> wc-haskell-exe.dashs
+stack exec -- wc-haskell-exe ../tmp > wc-haskell-exe.output
